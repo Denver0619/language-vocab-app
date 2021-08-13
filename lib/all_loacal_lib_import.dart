207cd0fb59bef5dddae39096c6275635396eb5ee
Denver0619 <janjan12345678910111213141516@gmail.com>
2021-08-13 10:34:32 +0800

    Added Article Lessons

--- lib/all_loacal_lib_import.dart
+++ lib/all_loacal_lib_import.dart
@@ -4,10 +4,7 @@ export 'tabs/dictionary.dart';
 export 'tabs/grammar.dart';
 export 'tabs/settings.dart';
 export 'navigation_route.dart';
-<<<<<<< HEAD
 export 'tabs/grammar/noun.dart';
 
 //packages
-=======
->>>>>>> 4a52318526db1bc8515332916b272b058c8d0e60
 export 'package:provider/provider.dart';
---
+++ lib/tabs/grammar/article.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../../constructor.dart';
+
+class ArticleLessons extends StatefulWidget {
+  const ArticleLessons({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _ArticleLessonsState createState() => _ArticleLessonsState();
+}
+
+class _ArticleLessonsState extends State<ArticleLessons> {
+  List<LessonTile> articleContents = [
+    LessonTile(lessonNum: '2.1', title: 'Definite Articles', path: ''),
+    LessonTile(lessonNum: '2.2', title: 'Usage of Definite Articles', path: ''),
+    LessonTile(lessonNum: '2.3', title: 'Indefinite Articles', path: ''),
+    LessonTile(
+        lessonNum: '2.4', title: 'Usage of Indefinite Articles', path: ''),
+    LessonTile(lessonNum: '2.5', title: 'Neuter Articles', path: '')
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${widget.title}'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: (BuildContext context, index) {
+                return ListTile(
+                    title: Text('${articleContents[index].lessonNum}' +
+                        ' ' +
+                        '${articleContents[index].title}'),
+                    onTap: () {
+                      Navigator.of(context)
+                          .pushNamed('${articleContents[index].path}');
+                    });
+              },
+              separatorBuilder: (BuildContext context, index) {
+                return Divider();
+              },
+              itemCount: articleContents.length,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
